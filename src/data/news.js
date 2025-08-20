d3b2c20df10864a9eb157dc6d18ec632ec64e031
RishanthRajendhran <rishanthrajendhran@outlook.com>
2025-08-20 14:48:39 -0400

    Updated News: VeriFastScore acceptance

--- src/data/news.js
+++ src/data/news.js
@@ -1,7 +1,8 @@
 export const news = [
+  { title:"VeriFastScore accepted to EMNLP Findings 2025!", date:"2025-08-20", link:"#"},
   { title:"Moved to UMD", date:"2025-01-15", link:"#"},
   { title:"Started PhD in CS at UMass Amherst", date:"2024-09-1", link:"#"},
   { title:"Graduated from Utah with an MSCS degree", date:"2024-05-03", link:"#"},
-  { title:"NAACL 2024 publication accepted!", date:"2024-04-05", link:"https://arxiv.org/abs/2311.09694"},
+  { title:"Whispers of Doubt accepted to NAACL 2024!", date:"2024-04-05", link:"https://arxiv.org/abs/2311.09694"},
   { title:"Graduated from IITM with an B.Tech in CSE degree", date:"2022-07-17", link:"#"},
 ];
